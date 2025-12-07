8bb3dce959ab0fc7d1e6f3fae78c18632afb95ef
Michal Trefil <michal.trefil@gmail.com>
2025-12-07 18:08:33 +0100

    Initial commit: Sent library only

---
+++ include/Sent.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "esp_rom_sys.h"
+#include <Arduino.h>
+#include <stdio.h>
+
+class SentClass
+{
+public:
+    void begin(long baud = 115200) { (void)baud; }
+
+    // print pro string
+    void print(const char *msg) { ets_printf("%s", msg); }
+
+    // print pro čísla
+    void print(unsigned long val) { ets_printf("%lu", val); }
+    void print(long val) { ets_printf("%ld", val); }
+    void print(int val) { ets_printf("%d", val); }
+    void print(float val, int precision = 2)
+    {
+        char buf[32];
+        dtostrf(val, 0, precision, buf);
+        ets_printf("%s", buf);
+    }
+
+    // println pro string
+    void println(const char *msg) { ets_printf("%s\n", msg); }
+    void println(unsigned long val) { ets_printf("%lu\n", val); }
+    void println(long val) { ets_printf("%ld\n", val); }
+    void println(int val) { ets_printf("%d\n", val); }
+    void println(float val, int precision = 2)
+    {
+        char buf[32];
+        dtostrf(val, 0, precision, buf);
+        ets_printf("%s\n", buf);
+    }
+
+    void println() { ets_printf("\n"); }
+};
+
+// globální objekt
+extern SentClass Sent;
---
+++ src/Sent.cpp
@@ -0,0 +1,3 @@
+#include "Sent.h"
+
+SentClass Sent;
